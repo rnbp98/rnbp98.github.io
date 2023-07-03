f5ea6b7db2914d0be608014cf49bb98ab87286a2
Nicolas Burbano <nicolas.rnbp@gmail.com>
2023-07-03 02:41:08 -0500

    music player prototipe

--- js/main.js
+++ js/main.js
@@ -1,3 +1,68 @@
+const playlists = [
+  {
+    id: "op001",
+    user: "Intellbg",
+    name: "Otaku mood",
+    creation_date: "2023-06-12",
+    last_reproduction: "2023-06-13",
+    img: "otaku.jpg",
+    song_url: [
+      "https://www.youtube.com/embed/AH4Vx5zz7Go?autoplay=1",
+      "https://www.youtube.com/watch?v=3eytpBOkOFA",
+      "https://www.youtube.com/watch?v=kzdJkT4kp-A",
+    ],
+  },
+  {
+    id: "op002",
+    user: "Intellbg",
+    name: "420",
+    creation_date: "2023-06-02",
+    last_reproduction: "2023-06-23",
+    img: "420.jpg",
+    song_url: [
+      "https://www.youtube.com/embed/58PCM-x7jlg?autoplay=1",
+      "https://www.youtube.com/watch?v=lIbWIqv-z38",
+    ],
+  },
+  {
+    id: "op003",
+    user: "Sarbg",
+    name: "M0",
+    creation_date: "2023-06-02",
+    last_reproduction: "2023-06-23",
+    img: "metal.jpg",
+    song_url: [
+      "https://www.youtube.com/embed/aoGiR5E8K_A?autoplay=1",
+      "https://www.youtube.com/watch?v=lIbWIqv-z38",
+    ],
+  },
+  {
+    id: "op004",
+    user: "Sarbg",
+    name: "BadBunny",
+    creation_date: "2023-06-02",
+    last_reproduction: "2023-06-23",
+    img: "bb.jpg",
+    song_url: [
+      "https://www.youtube.com/embed/pQ_mv3usXqk?autoplay=1",
+      "https://www.youtube.com/watch?v=Yeu3uu8VAe8",
+      "https://www.youtube.com/watch?v=lIbWIqv-z38",
+    ],
+  },
+  {
+    id: "op005",
+    user: "Uwumang",
+    name: "LateniteJAzz",
+    creation_date: "2023-06-02",
+    last_reproduction: "2023-06-23",
+    img: "jazz.jpg",
+    song_url: [
+      "https://www.youtube.com/embed/4kLOu3813BU?autoplay=1",
+      "https://www.youtube.com/watch?v=Yeu3uu8VAe8",
+      "https://www.youtube.com/watch?v=lIbWIqv-z38",
+    ],
+  },
+];
 function getPlaylists() {
   //TODO: Correct url api call
   fetch("http://127.0.0.1:3000/api/playlists")
@@ -32,6 +97,28 @@ function getPlaylists() {
     .catch((error) => {
       console.error("Error:", error);
     });
+
+  playlists.forEach((data) => {
+    const tableBody = document.getElementById("playlists_table");
+    const row = document.createElement("tr");
+    const nameCell = document.createElement("td");
+    nameCell.textContent = data.name;
+    row.appendChild(nameCell);
+    const creationCell = document.createElement("td");
+    creationCell.textContent = data.creation_date;
+    row.appendChild(creationCell);
+    const lastCell = document.createElement("td");
+    lastCell.textContent = data.last_reproduction;
+    row.appendChild(lastCell);
+    const actionCell = document.createElement("td");
+    actionCell.innerHTML = `
+    <span class="material-symbols-outlined" onclick="playlist('${data.id}')">play_arrow</span>
+    <span class="material-symbols-outlined" onclick="openEdit('${data.id}')">edit</span>
+    <span class="material-symbols-outlined" onclick="openDelete('${data.id}')">delete</span>
+    `;
+    row.appendChild(actionCell);
+    tableBody.appendChild(row);
+  });
 }
 
 let urlCount = 1;
@@ -79,6 +166,18 @@ function openEdit(id) {
     .catch((error) => {
       console.error("Error:", error);
     });
+  const jsonData = playlists.filter(function (playlist) {
+    return playlist.id === id;
+  });
+  const data = jsonData[0];
+  $("#name_e").val(data.name);
+  data.song_url.forEach((song, index) => {
+    if (index == 0) {
+      $("#url_e").val(song);
+    } else {
+      addUrlField("urlsContainerEdit", song);
+    }
+  });
   $("#editModal").modal("toggle");
 }
 
@@ -92,12 +191,18 @@ function openDelete(id) {
     })
     .then((jsonData) => {
       const data = jsonData[0];
-      console.log('Playlist: '+data.name)
-      $("#delete_modal_content").html('Playlist: '+data.name)
+      console.log("Playlist: " + data.name);
+      $("#delete_modal_content").html("Playlist: " + data.name);
     })
     .catch((error) => {
       console.error("Error:", error);
     });
+  const jsonData = playlists.filter(function (playlist) {
+    return playlist.id === id;
+  });
+  const data = jsonData[0];
+  console.log("Playlist: " + data.name);
+  $("#delete_modal_content").html("Playlist: " + data.name);
   $("#deleteModal").modal("toggle");
 }
 
@@ -127,3 +232,14 @@ document
       `;
     urlCount = 1;
   });
+
+function playlist(id) {
+  const jsonData = playlists.filter(function (playlist) {
+    return playlist.id === id;
+  });
+  const data = jsonData[0];
+  $(".music_controller").removeClass("d-none");
+  $("#active_playlist").html(data.name);
+  $("#playlist_img").attr("src", "./imgs/" + data.img);
+  document.getElementById("music_player").src=data.song_url[0]
+}
