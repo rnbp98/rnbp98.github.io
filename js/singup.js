ca44a8c3070f66177db6f5ef4e60e85dd6c0c4f4
Nicolas Burbano <nicolas.rnbp@gmail.com>
2023-07-03 08:33:02 -0500

    register validation

--- js/singup.js
+++ js/singup.js
@@ -1,1 +1,38 @@
+function validatePassword() {
+    var passwordInput = document.getElementById("password").value;
+    var passwordInput2 = document.getElementById("password2").value;
+    document.getElementById("passwordError").innerHTML ="";
+    if (passwordInput.length < 8) {
+      document.getElementById("passwordError").textContent =
+        "La contraseña debe tener al menos 8 caracteres";
+      return false;
+    }
+    if (!/\d/.test(passwordInput)) {
+      document.getElementById("passwordError").textContent =
+        "La contraseña debe tener al menos un número";
+      return false;
+    }
+    if (!/[A-Z]/.test(passwordInput)) {
+      document.getElementById("passwordError").textContent =
+        "La contraseña debe tener al menos una mayúscula";
+      return false;
+    }
+  
+    if (!/[!@#$%^&*]/.test(passwordInput)) {
+      document.getElementById("passwordError").textContent =
+        "La contraseña debe tener al menos un carácter especial (!@#$%^&*).";
+      return false;
+    }
+    if (passwordInput !== passwordInput2) {
+      document.getElementById("passwordError").textContent =
+        "La contraseña debe coincidir.";
+      return false;
+    }
+    return true;
+  }
 
+$("#singup").on('submit',(evt)=>{
+    evt.preventDefault()
+    validatePassword()
+    document.getElementById("singup").reset()
+})
